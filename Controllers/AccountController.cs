803f0abf5fc0b0c66ba6159915e58e8cbcf1e513
Andhika Pramana <andhikapramana101@gmail.com>
2022-09-29 08:25:20 +0700

    Create authentication & authorization @client

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using API.ViewModels;
+using Client.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        HttpClient HttpClient;
+        string address;
+
+        public AccountController()
+        {
+            this.address = "https://localhost:5001/api/Account/";
+            HttpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address)
+            };
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+            var result = HttpClient.PostAsync(address, content).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<ResponseClient>(await result.Content.ReadAsStringAsync());
+                HttpContext.Session.SetString("Role", "cek");
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
